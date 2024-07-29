fb6dcd98404229065b407ff61af1c046e609433b
정서익 <166768925+SeoikJung@users.noreply.github.com>
2024-07-29 17:55:16 +0900

    [Feat] data update

---
+++ metadata/key.py
@@ -0,0 +1,220 @@
+RACES = {
+    "indian": [  # 인도인 관련 인종 그룹
+        "Indian",  # 인도 출신의 사람들
+        "South Asian",  # 남아시아 출신의 사람들
+        "Asian Indian",  # 아시아 대륙의 인도 출신 사람들
+        "Desi",  # 남아시아 출신 사람들
+        "Aryan",  # 역사적으로 인도유럽어족에 속하는 사람들
+        "Mongoloid",  # 동아시아, 북아시아, 중아시아 인종군
+        "Dravidian",  # 남인도 지역의 원주민 민족
+        "Indo-Aryan",  # 인도 북부 및 중부 지역의 언어 및 민족 집단
+        "Indo-European",  # 인도와 유럽의 언어적 기원을 공유하는 민족
+        "Indigenous"  # 인도의 원주민 또는 토착민
+    ],
+    "white": [  # 백인 관련 인종 그룹
+        "White",
+        "Caucasian",  # 일반적으로 백인을 지칭하는 용어
+        "European",  # 유럽 대륙 출신의 사람들
+        "Anglo",  # 영국 출신이나 영국 혈통의 사람들
+        "Nordic",  # 북유럽 출신의 사람들
+        "Slavic",  # 동유럽 및 중부 유럽의 슬라브계 민족
+        "Mediterranean",  # 지중해 연안 국가 출신의 사람들
+        "Celtic",  # 아일랜드, 스코틀랜드, 웨일즈, 브리타니 출신의 사람들
+        "Teutonic",  # 게르만족의 후손을 지칭하는 용어
+        "Germanic",  # 게르만족에 속하는 사람들
+        "Irish",  # 아일랜드 출신의 사람들
+        "Scandinavian",  # 스칸디나비아 반도 출신의 사람들
+        "Baltic"  # 발트해 연안 국가 출신의 사람들
+    ],
+    "hispanic": [  # 히스패닉 관련 인종 그룹
+        "Hispanic",  # 스페인어를 사용하는 라틴 아메리카 및 스페인 출신의 사람들
+        "Latino",  # 라틴 아메리카 출신의 사람들
+        "Spanish",  # 스페인 출신의 사람들
+        "Mexican",  # 멕시코 출신의 사람들
+        "Puerto Rican",  # 푸에르토리코 출신의 사람들
+        "Cuban",  # 쿠바 출신의 사람들
+        "Dominican",  # 도미니카 공화국 출신의 사람들
+        "Central American",  # 중앙 아메리카 지역 출신의 사람들
+        "South American",  # 남아메리카 지역 출신의 사람들
+        "Chicano",  # 미국 내 멕시코계 미국인을 지칭하는 용어
+        "Native American" # 북미 인디언
+    ],
+    "arab": [  # 아랍인 관련 인종 그룹
+        "Arab",  # 아랍어를 사용하는 사람들
+        "Middle Eastern",  # 중동 지역 출신의 사람들
+        "North African",  # 북아프리카 지역 출신의 사람들
+        "Egyptian",  # 이집트 출신의 사람들
+        "Saudi",  # 사우디아라비아 출신의 사람들
+        "Syrian",  # 시리아 출신의 사람들
+        "Iraqi",  # 이라크 출신의 사람들
+        "Jordanian",  # 요르단 출신의 사람들
+        "Lebanese",  # 레바논 출신의 사람들
+        "Palestinian",  # 팔레스타인 출신의 사람들
+        "Moroccan",  # 모로코 출신의 사람들
+        "Algerian",  # 알제리 출신의 사람들
+        "Tunisian",  # 튀니지 출신의 사람들
+        "Emirati",  # 아랍에미리트 출신의 사람들
+        "Qatari",  # 카타르 출신의 사람들
+        "Kuwaiti",  # 쿠웨이트 출신의 사람들
+        "Yemeni",  # 예멘 출신의 사람들
+        "Omani",  # 오만 출신의 사람들
+        "Libyan",  # 리비아 출신의 사람들
+        "Bahraini"  # 바레인 출신의 사람들
+    ],
+    "black": [  # 흑인 관련 인종 그룹
+        "African",  # 아프리카 출신의 사람들
+        "African American",  # 아프리카계 미국인
+        "Black",  # 일반적으로 흑인을 지칭하는 용어
+        "Afro-Caribbean",  # 아프리카계 카리브해 출신의 사람들
+        "Nigerian",  # 나이지리아 출신의 사람들
+        "Ethiopian",  # 에티오피아 출신의 사람들
+        "Somali",  # 소말리아 출신의 사람들
+        "Ghanaian",  # 가나 출신의 사람들
+        "Kenyan",  # 케냐 출신의 사람들
+        "South African",  # 남아프리카 공화국 출신의 사람들
+        "Jamaican",  # 자메이카 출신의 사람들
+        "Haitian"  # 아이티 출신의 사람들
+    ],
+    "asian": [  # 아시아인 관련 인종 그룹
+        "East Asian",  # 동아시아 출신의 사람들
+        "asian",  # 아시아 출신의 사람들
+        "Chinese",  # 중국 출신의 사람들
+        "Japanese",  # 일본 출신의 사람들
+        "Korean",  # 한국 출신의 사람들
+    ]
+}
+
+SEXS = {
+    "male": [  # 남성을 의미하는 단어들
+        "Man",  # 남자
+        "Boy",  # 소년
+        "Gentleman",  # 신사
+        "Guy",  # 남자
+        "Male",  # 남성
+        "Lad",  # 청년
+        "Gent",  # 신사
+        "Bloke",  # (영국) 남자
+        "Chap",  # (영국) 남자
+        "Dude",  # (미국) 남자
+        "Mister"  # 남자, 신사
+    ],
+    "female": [  # 여성을 의미하는 단어들
+        "Woman",  # 여자
+        "Girl",  # 소녀
+        "Lady",  # 숙녀
+        "Female",  # 여성
+        "Dame",  # (고어적 또는 귀부인) 여성
+        "Miss",  # 아가씨
+        "Madam",  # 여사
+        "Chick",  # (비격식) 젊은 여자
+        "Lass",  # (영국) 소녀
+        "Babe"  # (비격식) 애정을 담아 여자
+    ]
+}
+
+AGES = [
+    "20 y.o",  # 20대
+    "30 y.o",  # 30대
+    "40 y.o"  # 40대
+]
+
+SKIN_TONES = [
+    "fair skin",  # 밝고 투명한 피부 톤
+    "light skin",  # 밝은 피부 색깔, fair보다 좀 더 색이 있는 피부
+    "medium skin",  # 중간 정도의 피부 톤
+    "olive skin",  # 올리브 톤의 피부
+    "tanned skin",  # 자연스럽게 태닝된 피부
+    "brown skin",  # 갈색 톤의 피부
+]
+
+SKIN_TEXTURES = [
+    "smooth skin",  # 결점이나 흉터가 없는 깨끗한 피부
+    "glowing skin",  # 빛나고 광택이 있는 피부
+    "matte skin",  # 무광택의 피부
+    "flawless skin",  # 잡티가 없는 피부
+    "oily skin",  # 기름진 피부
+    "dry skin",  # 건조한 피부
+]
+
+HAIR_COLORS = [
+    "black hair",  # 검은색 머리
+    "brown hair",  # 갈색 머리
+    "blonde hair",  # 금발
+    "auburn hair",  # 적갈색 머리
+    "red hair",  # 빨간 머리
+    "ginger hair",  # 빨간 머리
+    "gray hair",  # 회색 머리
+    "white hair",  # 흰색 머리
+    "silver hair"  # 은색 머리
+]
+
+HAIR_STYLES = [
+    "short hair",  # 짧은 머리 스타일
+    "medium hair",  # 중간 머리 스타일
+    "long hair",  # 긴 머리 스타일
+    "bald",  # 대머리
+    "straight hair",  # 직모
+    "wavy hair",  # 부드럽게 구부러진 머리
+    "curly hair",  # 인위적이고 좀 더 촘촘하게 구부러진 머리
+    "ponytail",  # 말총 머리
+    "bun",  # 머리를 둥글게 말아 올린 스타일
+    "braids",  # 땋은 머리
+    "bob cut",  # 목 주위에서 끝나는 짧은 단발머리 스타일
+    "side part"  # 가르마를 비대칭으로 하여 정리한 스타일
+]
+
+EYE_COLORS = [
+    "blue eyes",  # 파란색 눈
+    "brown eyes",  # 갈색 눈
+    "green eyes",  # 녹색 눈
+    "black eyes",  # 검은색 눈
+    "hazel eyes",  # 녹색과 갈색의 혼합
+    "grey eyes",  # 회색 눈
+    "amber eyes"  # 호박색 눈
+]
+
+FILM_TYPES = [
+    "night vision",  # 밤에 촬영한 사진 느낌
+    "35mm film",  # 35mm 필름 카메라 느낌
+    "instax",  # 후지 인스탁스 즉석카메라로 촬영한 느낌
+    "camcorder",  # 캠코더로 촬영한 느낌
+    "lomo",  # 로모로 촬영한 느낌
+    "vintage",  # 빈티지
+    "Nikon D750",  # 니콘 D750으로 촬영한 느낌
+    "Kodak Gold 200",  # 코닥 골드 200 필름으로 인화한 느낌
+    "tintype",  # 19세기 옛날사진같은 느낌
+]
+
+DISPLAY_TYPES = [
+    "4k",  # 4K
+    "8k",  # 8K
+    "HD",  # HD
+    "Full-HD",  # 풀HD
+    "Ultra-HD",  # 울트라HD
+    "super-resolution display",  # 초고해상도 디스플레이
+    "megapixel display"  # 메가픽셀 디스플레이
+]
+
+LIGHTING_TYPES = [
+    "spotlight",  # 스포트라이트
+    "frontlight",  # 전조등
+    "backlight",  # 백라이트
+    "rim lighting",  # 림 조명
+    "bright",  # 밝게
+    "ultrabright",  # 매우 밝게
+    "artificial lighting",  # 인공 조명
+    "natural lighting",  # 자연광
+    "fluorescent"  # 형광등
+]
+
+ADDITIONAL_ADJECTIVES = [
+    "elegant",  # 우아한
+    "mystical",  # 신비로운
+    "colorful",  # 화려한
+    "intricate",  # 뒤얽힌
+    "detailed",  # 상세한
+    "fantasy",  # 공상적인
+    "floral",  # 꽃으로 가득한
+    "smooth",  # 매끄러운
+    "cute"  # 귀여운
+]
---
+++ metadata/metadata.py
@@ -0,0 +1,84 @@
+import json
+from metadata.translations import TRANSLATIONS
+import os
+import gradio as gr
+from datetime import datetime
+
+def get_select_gallery(evt: gr.SelectData):
+
+    image_path = evt.value['image']['path']  # 이미지 경로를 가져옴
+    orig_name = evt.value['image']['orig_name']  # 원래 이름을 가져옴
+    
+    # print(f"==>> evt.selected: {evt.selected}") # True
+    # print(f"==>> evt._data: {evt._data}") # {'index': 1, 'value': None}
+    # print(f"You selected {evt.value} at {evt.index} from {evt.target}") # You selected None at 1 from gallery
+    print(f"Image path: {image_path}")  # 이미지 경로 출력
+    print(f"Original name: {orig_name}")  # 원래 이름 출력
+    
+    today = datetime.now().strftime("%Y-%m-%d")
+    new_imgae_path = os.path.join(f"result/portrait/{today}", orig_name)
+    new_json_path = os.path.join(f"result/portrait/{today}", orig_name.replace('.png', '.json'))
+    new_path = (new_imgae_path, new_json_path)
+    print(f"New path: {new_path}")  
+
+    return image_path, new_path
+
+def get_translated_metadata(metadata):
+    keys_to_translate = [
+        "RACES", "AGES", "SEXS", "SKIN_TEXTURES", "SKIN_TONES",
+        "HAIR_COLORS", "HAIR_STYLES", "EYE_COLORS", "FILM_TYPES",
+        "DISPLAY_TYPES", "LIGHTING_TYPES", "ADDITIONAL_ADJECTIVES"
+    ]
+    translated_values = []
+    for key in keys_to_translate:
+        if key in metadata and metadata[key] is not None:
+            value = metadata[key]
+            translated_value = TRANSLATIONS.get(value, value)
+            translated_values.append(translated_value)
+    return translated_values
+
+def load_and_translate_metadata(evt: gr.SelectData):
+    orig_name = evt.value['image']['orig_name']
+    today = datetime.now().strftime("%Y-%m-%d")
+    new_json_path = os.path.join(f"result/portrait/{today}", orig_name.replace('.png', '.json'))
+    
+    try:
+        with open(new_json_path, "r", encoding="utf-8") as f:
+            metadata = json.load(f)
+            translated_values = get_translated_metadata(metadata)
+            result_string = ', '.join(translated_values)
+            return result_string
+    except FileNotFoundError:
+        print(f"Error: {new_json_path} not found")
+        return None
+    
+def load_and_translate_metadata_var(evt: gr.SelectData):
+    orig_name = evt.value['image']['orig_name']
+    today = datetime.now().strftime("%Y-%m-%d")
+    new_json_path = os.path.join(f"result/portrait/{today}", orig_name.replace('.png', '.json'))
+    
+    try:
+        with open(new_json_path, "r", encoding="utf-8") as f:
+            metadata = json.load(f)
+            translated_values = get_translated_metadata(metadata)
+            result_string = ', '.join(translated_values)
+            values = translated_values
+            print(translated_values)
+            return (
+        gr.update(),  # races 값을 변경하지 않음
+        gr.update(value=values[1]),  # ages
+        gr.update(),  # sex 값을 변경하지 않음
+        gr.update(value=values[3]),  # skin_texture
+        gr.update(value=values[4]),  # skin_tone
+        gr.update(value=values[5]),  # hair_color
+        gr.update(value=values[6]),  # hair_style
+        gr.update(value=values[7]),  # eyes_color
+        gr.update(value=values[8]),  # camera
+        gr.update(value=values[9]),  # display
+        gr.update(value=values[10]), # light
+        gr.update(value=values[11])  # adj
+    )
+            return result_string
+    except FileNotFoundError:
+        print(f"Error: {new_json_path} not found")
+        return None
